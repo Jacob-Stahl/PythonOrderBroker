08d3a2b7cd5eff32393c21d424112f3a8e699624
Jacob Stahl <50686850+Jacob-Stahl@users.noreply.github.com>
2025-11-01 17:41:22 -0500

    WIP agent breeder

--- pybroker/agents/agent_breeder.py
+++ pybroker/agents/agent_breeder.py
@@ -7,5 +7,31 @@ from pybroker.models import *
 import random
 import math
 import numpy as np
+from typing import ClassVar
+
+class AgentBreeder():
+    def __init__(self,
+                 agent_cls: type,
+                 traderIds: set[int]   
+                 ) -> None:
+
+        self.pool_size = len(traderIds)
+        self.agents: dict[int, Agent] = {}
+
+        # initialize all agents
+        for i in traderIds:
+            agent = agent_cls()
+            assert issubclass(Agent, agent)
+            self.agents[i] = agent
 
 
+    def cull(self) -> None:
+        raise NotImplementedError()
+
+    @staticmethod
+    def _should_kill(account: Account) -> bool:
+        """kill if tradable balance is less than 1000"""
+        return account.tradable_balance_cents() < 1000
+    
+
+    
