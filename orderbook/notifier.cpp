59b8609c0f9a40e66ae5251814a8317492f8d7a5
Jacob Stahl <50686850+Jacob-Stahl@users.noreply.github.com>
2025-12-24 10:36:25 -0600

    added seperate notifier class

---
+++ orderbook/notifier.cpp
@@ -0,0 +1,15 @@
+#include "notifier.h"
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <format>
+
+void MockNotifier::notifyOrderPlaced(const Order& order){
+    std::cout << "Order " << order.ordId << " for trader " << order.traderId << " placed";
+}
+void MockNotifier::notifyOrderPlacementFailed(const Order& order, std::string reason){
+    std::cout << "Order " << order.ordId << " for trader " << order.traderId << " failed. Reason: " << reason;
+}
+void MockNotifier::notifyOrderMatched(){
+    std::cout << "Matched!";
+}
---
+++ orderbook/notifier.h
@@ -0,0 +1,20 @@
+#include "order.h"
+#include <vector>
+
+
+class INotifier{
+    public:
+        virtual void notifyOrderPlaced(const Order& order);
+        virtual void notifyOrderPlacementFailed(const Order& order, std::string reason);
+        virtual void notifyOrderMatched();
+};
+
+
+/// @brief Notifier used for testing
+class MockNotifier: public INotifier{
+    public:
+        void notifyOrderPlaced(const Order& order);
+        void notifyOrderPlacementFailed(const Order& order, std::string reason);
+        void notifyOrderMatched();
+};
+
