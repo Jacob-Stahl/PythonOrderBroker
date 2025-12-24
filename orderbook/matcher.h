165840c316782bc299f44393033e2e1f72fc0d7c
Jacob Stahl <50686850+Jacob-Stahl@users.noreply.github.com>
2025-12-24 10:39:41 -0600

    added comment about match time and message ordering

--- orderbook/matcher.h
+++ orderbook/matcher.h
@@ -55,6 +55,7 @@ class Matcher{
         Spread Matcher::getSpread();
 };
 
+// TODO time of match might be important for message ordering down stream
 struct Match{
     Order order;
     std::vector<Order> matchingOrders;
